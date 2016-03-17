b124b6f763402166e4acaa44fd5543637977c892
root <root@xubuntu>
2016-03-17 13:54:57 +0700

    FORCE_SSL_ADMIN

--- .openshift/config/wp-config.php
+++ .openshift/config/wp-config.php
@@ -133,3 +133,4 @@ require_once(ABSPATH . 'wp-settings.php');
 //define url
 define('WP_HOME','http://www.winardiaris.xyz');
 define('WP_SITEURL','http://www.winardiaris.xyz');
+define('FORCE_SSL_ADMIN', false);
